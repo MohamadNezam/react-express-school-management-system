3250f7b9fb16fcfa9fc17709da803a128f71d4c0
Mohamad Nezam <mohamad.nezam@mail.polimi.it>
2021-07-13 14:56:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.API_PORT || 5000;
 
 app.use(bodyParser.json());
 app.get('/api/hello', (req, res) => {
